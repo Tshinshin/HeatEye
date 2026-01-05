4e6c587847d1c7a561867eda3cb44a50bac4ea60
Hiroyuki TAKESHITA <hiroyuki.takeshita@3ds.com>
2026-01-05 13:07:11 +0900

    Modified top page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="p-10">
-      <h1 className="text-3xl font-bold mb-4">Hello shadcn + Next.js + Tailwind v4</h1>
-      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold mb-4">
+        Hello shadcn + Next.js + Tailwind v4
+      </h1>
+
+      <Link href="/dashboard">
+        <Button>Click me</Button>
+      </Link>
     </main>
   );
 }

1fbb4c68f6f5cc26e7704a9ed433817a34a85dd0
Hiroyuki TAKESHITA <hiroyuki.takeshita@3ds.com>
2026-01-06 11:44:39 +0900

    login/page.tsx updated

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,9 +1,12 @@
-import { LoginForm } from "@/components/login-form"
+import { Suspense } from "react";
+import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
-  )
+  );
 }
